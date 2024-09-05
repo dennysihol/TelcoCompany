911224a89f082bbbc602fd3e1f99348bc08e31ed
Haikal <haikalharin1998@gmail.com>
2024-09-05 16:17:06 +0700

    add swiping partner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,11 +32,101 @@
       <p>Keamanan informasi, perlindungan privasi ganda untuk mencegah kebocoran informasi</p>
     </div>
   </section>
+  <section class="partner-section">
+    <div class="partner-text">
+      <h3>Mitra Bisnis Kami</h3>
+    </div>
+    <div class="partner-swiper">
+      <swiper
+          :slidesPerView="4"
+          :centeredSlides="false"
+          :slidesPerGroupSkip="0"
+          :grabCursor="true"
+          :spaceBetween="0"
+          :keyboard="{
+        enabled: true,
+      }"
+          :breakpoints="{
+        '769': {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+        },
+      }"
+          :scrollbar="false"
+          :navigation="false"
+
+          :autoplay="{
+        delay: 3000, // Delay between slides in milliseconds
+        disableOnInteraction: false, // Continue autoplay after interaction
+      }"
+          :modules="modules"
+          class="mySwiper"
+      >
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_1.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_2.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_3.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_4.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_5.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_6.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_7.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_8.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_9.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_10.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_11.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/image/ic_partner_12.png" />
+        </swiper-slide>
+      </swiper>
+    </div>
+  </section>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Autoplay, Pagination, Navigation, Scrollbar, Keyboard } from 'swiper/modules';
+
+// If you use pagination
 export default {
   name: 'Home',
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Keyboard, Scrollbar, Navigation, Pagination, Autoplay],
+    };
+  },
+
 }
 </script>
 
@@ -81,7 +171,6 @@ export default {
   color: #666;
 }
 
-
 .icon img {
   width: 60px; /* Adjust based on your icon size */
   height: auto;
@@ -103,4 +192,49 @@ p {
   background-color: #ddd;
   margin: 0 20px;
 }
+
+.partner-section {
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  margin-left: 300px;
+}
+
+.partner-text {
+  flex: 0 0 200px;
+  text-align: left;
+  padding-right: 20px;
+}
+
+.partner-swiper {
+  flex: 1;
+}
+
+body {
+  font-family: Arial, sans-serif;
+}
+
+.swiper-container {
+  width: 100%;
+  height: 150px;
+}
+
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.swiper-slide img {
+  max-width: 177px; /* Adjust the size of the logos */
+  max-height: 56px;
+  object-fit: contain;
+}
+
+.mySwiper {
+  max-width: 814px;
+  padding-top: 20px;
+}
+
 </style>
