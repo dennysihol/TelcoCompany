fc69d4fa66220811f751a5da142a8e98a57289a1
Haikal <haikalharin1998@gmail.com>
2024-09-09 11:33:00 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,76 +13,8 @@
   <section class="ud-hero" id="home">
     <Information/>
     <Advantages/>
+    <BusinessPartners/>
     <Article/>
-    <section class="partner-section">
-      <div class="partner-text">
-        <h3>Mitra Bisnis Kami</h3>
-      </div>
-      <div class="partner-swiper">
-        <swiper
-            :slidesPerView="4"
-            :centeredSlides="false"
-            :slidesPerGroupSkip="0"
-            :grabCursor="true"
-            :spaceBetween="0"
-            :keyboard="{
-        enabled: true,
-      }"
-            :breakpoints="{
-        '769': {
-          slidesPerView: 4,
-          slidesPerGroup: 4,
-        },
-      }"
-            :scrollbar="false"
-            :navigation="false"
-
-            :autoplay="{
-        delay: 3000, // Delay between slides in milliseconds
-        disableOnInteraction: false, // Continue autoplay after interaction
-      }"
-            :modules="modules"
-            class="mySwiper"
-        >
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_1.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_2.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_3.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_4.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_5.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_6.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_7.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_8.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_9.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_10.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_11.png" />
-          </swiper-slide>
-          <swiper-slide>
-            <img src="@/assets/image/ic_partner_12.png" />
-          </swiper-slide>
-        </swiper>
-      </div>
-    </section>
   </section>
 </template>
 
@@ -101,6 +33,7 @@ import { Autoplay, Pagination, Navigation, Scrollbar, Keyboard } from 'swiper/mo
 import {
   Advantages,
   Article,
+  BusinessPartners,
   Information,
 } from './home/index.js';
 
@@ -112,6 +45,7 @@ export default {
     Advantages,
     Article,
     Information,
+    BusinessPartners
   },
   setup() {
     return {
@@ -162,55 +96,7 @@ p {
   display: block;
 }
 
-.divider {
-  width: 1px;
-  background-color: #ddd;
-  margin: 0 20px;
-}
 
 
-.partner-section {
-  display: flex;
-  align-items: center;
-  margin-top: 40px;
-  margin-left: 300px;
-}
-
-.partner-text {
-  flex: 0 0 200px;
-  text-align: left;
-  padding-right: 20px;
-}
-
-.partner-swiper {
-  flex: 1;
-}
-
-body {
-  font-family: Arial, sans-serif;
-}
-
-.swiper-container {
-  width: 100%;
-  height: 150px;
-}
-
-.swiper-slide {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-.swiper-slide img {
-  max-width: 177px; /* Adjust the size of the logos */
-  max-height: 56px;
-  object-fit: contain;
-}
-
-.mySwiper {
-  max-width: 814px;
-  padding-top: 20px;
-}
 
 </style>
