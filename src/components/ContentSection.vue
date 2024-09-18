40ca587bc3168519ca1dccb67d9c32e5e36af58a
Haikal <haikalharin1998@gmail.com>
2024-09-18 17:07:42 +0700

    update

---
+++ src/components/ContentSection.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="section">
+    <h2>{{ sectionTitle }}</h2>
+    <div class="profiles">
+      <ProfileCard v-for="profile in profiles" :key="profile.name" :name="profile.name" :title="profile.title" :image="profile.image" />
+    </div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+import ProfileCard from './ProfileCard.vue';
+
+export default {
+  components: {
+    ProfileCard,
+  },
+  props: {
+    sectionTitle: String,
+    profiles: Array,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+.section {
+  padding: 20px;
+}
+.profiles {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+h2 {
+  margin-bottom: 10px;
+}
+p {
+  margin-top: 15px;
+}
+</style>
--- src/views/Tentang.vue
+++ src/views/Tentang.vue
@@ -1,13 +1,32 @@
 <template>
-    <section id="tentang">
-      <h1>Tentang</h1>
-    </section>
+
+  <div id="app">
+    <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel"
+         :data-bs-interval="intervalBanner" data-bs-pause="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="@/assets/image/home-banner-3.png" class="d-block w-100" alt="home-banner-3">
+        </div>
+      </div>
+    </div>
+    <Navbar />
+    <!-- Rest of your app -->
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'Tentang',
-}
-</script>
+import Navbar from "@/components/Navbar.vue";
+import {Swiper, SwiperSlide} from "swiper/vue";
+import {Advantages, Information} from "@/views/home/index.js";
 
-<style scoped></style>
+export default {
+  name: "About",
+  components: {
+    Information,
+    Swiper,
+    SwiperSlide,
+    Advantages,
+    Navbar,
+  },
+};
+</script>
