0df477c8ca71c292cda9a46e6a362540a09fa9da
subki <subki@stanfordtek.com>
2024-09-30 17:51:48 +0700

    feat: adding UI Blog header, button header and card blog.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,13 +1,74 @@
 <template>
-    <section id="blog">
-      <h1>Blog</h1>
-    </section>
+  <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel"
+       :data-bs-interval="intervalBanner" data-bs-pause="false">
+    <div class="carousel-inner">
+      <div :class="index === 0 ? 'carousel-item active' : 'carousel-item'" v-for="(item, index) in dataBanner" :key="item.id">
+        <img :src="item.urlImage" class="d-block w-100" :alt="'home-banner-'+item.id">
+      </div>
+    </div>
+  </div>
+  <section class="ud-hero" id="blog">
+    <Article/>
+  </section>
 </template>
 
 <script>
+import {
+  Article,
+} from './blog/index.js';
 export default {
-    name: 'Blog',
+  name: 'Blog',
+  components: {
+    Article,
+  },
+  data() {
+    return {
+      intervalBanner: 8000,
+      dataBanner: [
+        {
+          "id": 1,
+          "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fbg-blog.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+        }
+      ],
+      loading: true,
+      error: null,
+    };
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .carousel-item {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh; /* Set this to the desired height for full screen */
+    display: flex !important;           /* Enables flexbox */
+    justify-content: center; /* Centers horizontally */
+    align-items: center;     /* Centers vertically */
+    text-align: start;      /* Optional: Centers text within the element */
+  }
+
+  .carousel-item img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+  }
+  @media (max-width: 768px) {
+    .carousel-item {
+      top: 7rem;
+      max-height: 80vh;
+
+      img {
+        height: 20rem;
+      }
+      
+      .banner-title {
+        padding: 10px;
+        font-size: 2rem;
+      }
+    }
+  }
+</style>
---
+++ src/views/blog/Article.vue
@@ -0,0 +1,227 @@
+<template>
+    <section id="blog" class="position-relative bg-blog">
+        <div class="container container-article position-relative vh-100">
+            <div class="row">
+                <div class="col-lg-10 article-gap pb-5">
+                    <div class="d-flex gap-3 justify-content-center mb-3">
+                        <button :class="tabSelected == 'article'? 'btn-custom btn-custom-2 active' : 'btn-custom btn-custom-2'" type="button" @click="handleTab('article')">Artikel</button>
+                        <button :class="tabSelected == 'event'? 'btn-custom btn-custom-2 active' : 'btn-custom btn-custom-2'" type="button" @click="handleTab('event')">Event</button>
+                    </div>
+                    <div class="row justify-content-start">
+                        <div class="col-md-6 col-xl-3 mb-4" v-for="item in (tabSelected == 'article' ? dataArticle : dataEvent)" :key="item.id">
+                            <div class="card-article d-flex flex-column gap-3 pb-3">
+                                <div class="card-article-image">
+                                    <img :src="item.urlImage" class="w-100" alt="item-article-1">
+                                </div>
+                                <div class="card-article-date d-flex flex-row gap-2">
+                                    <img class="card-article-date-icon" src="@/assets/icon/calendar-icon.png">
+                                    {{ item.location }}, {{ item.dateArticle }}
+                                </div>
+                                <div class="card-article-title">{{ item.titleArticle }}</div>
+                                <div class="card-article-read-more">
+                                    Lebih lanjut 
+                                    <font-awesome-icon :icon="['fas', 'chevron-right']" size="xs"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <!-- <button class="btn-custom btn-custom-1" type="button">Lihat Semua</button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'Blog',
+    data() {
+        return {
+            tabSelected: "article",
+            dataArticle: [
+                {
+                    "id": 1,
+                    "titleArticle": 'Obrolan Pintar "Build Your Future, Be Smart About Money"',
+                    "location": "Jakarta",
+                    "dateArticle": "26 Mar 2023",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-1.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 2,
+                    "titleArticle": 'Muda Paham Fintech "Inovasi Keuangan Untuk Generasi Muda"',
+                    "location": "Bandar Lampung",
+                    "dateArticle": "13 Maret 2022",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-2.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 3,
+                    "titleArticle": 'OJK Fintech Days Palembang 2019 Otoritas Jasa Keuangan dan',
+                    "location": "Palembang",
+                    "dateArticle": "2-3 Mei 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-3.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 4,
+                    "titleArticle": 'Seminar Pendidikan Peran Teknologi Komunikasi dalam Bisnis Financial',
+                    "location": "Bandung",
+                    "dateArticle": "30 April 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-4.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 5,
+                    "titleArticle": 'Obrolan Pintar "Build Your Future, Be Smart About Money"',
+                    "location": "Jakarta",
+                    "dateArticle": "26 Mar 2023",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-1.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 6,
+                    "titleArticle": 'Muda Paham Fintech "Inovasi Keuangan Untuk Generasi Muda"',
+                    "location": "Bandar Lampung",
+                    "dateArticle": "13 Maret 2022",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-2.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 7,
+                    "titleArticle": 'OJK Fintech Days Palembang 2019 Otoritas Jasa Keuangan dan',
+                    "location": "Palembang",
+                    "dateArticle": "2-3 Mei 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-3.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 8,
+                    "titleArticle": 'Seminar Pendidikan Peran Teknologi Komunikasi dalam Bisnis Financial',
+                    "location": "Bandung",
+                    "dateArticle": "30 April 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-4.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 9,
+                    "titleArticle": 'Obrolan Pintar "Build Your Future, Be Smart About Money"',
+                    "location": "Jakarta",
+                    "dateArticle": "26 Mar 2023",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-1.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 10,
+                    "titleArticle": 'Muda Paham Fintech "Inovasi Keuangan Untuk Generasi Muda"',
+                    "location": "Bandar Lampung",
+                    "dateArticle": "13 Maret 2022",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-2.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 11,
+                    "titleArticle": 'OJK Fintech Days Palembang 2019 Otoritas Jasa Keuangan dan',
+                    "location": "Palembang",
+                    "dateArticle": "2-3 Mei 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-3.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+            ],
+            dataEvent: [
+                {
+                    "id": 1,
+                    "titleArticle": 'Seminar Pendidikan Peran Teknologi Komunikasi dalam Bisnis Financial',
+                    "location": "Bandung",
+                    "dateArticle": "30 April 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-4.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 2,
+                    "titleArticle": 'OJK Fintech Days Palembang 2019 Otoritas Jasa Keuangan dan',
+                    "location": "Palembang",
+                    "dateArticle": "2-3 Mei 2019",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-3.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 3,
+                    "titleArticle": 'Muda Paham Fintech "Inovasi Keuangan Untuk Generasi Muda"',
+                    "location": "Bandar Lampung",
+                    "dateArticle": "13 Maret 2022",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-2.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                },
+                {
+                    "id": 4,
+                    "titleArticle": 'Obrolan Pintar "Build Your Future, Be Smart About Money"',
+                    "location": "Jakarta",
+                    "dateArticle": "26 Mar 2023",
+                    "urlImage": "https://firebasestorage.googleapis.com/v0/b/pinjamduit-84ca8.appspot.com/o/pjdweb%2Fitem-article-1.png?alt=media&token=07bd71fa-5516-4f61-8da7-60c92d729879"
+                }
+            ],
+            loading: false,
+            error: null,
+        };
+    },
+    methods: {
+        handleTab(nameTab) {
+            this.tabSelected = nameTab;
+        }
+    }
+}
+</script>
+<style scoped>
+    .bg-blog {
+        background: none;
+        top: -18rem;
+        z-index: 1;
+    }
+    .container-article {
+        padding: 5rem 0 5rem 0;
+    }
+    .article-gap {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+    }
+    .card-article {
+        border: 1px solid rgba(217, 217, 217, 1);
+        background: rgba(255, 255, 255, 1);
+        border-radius: 10px;
+        padding: 0 0 10px 2rem;
+
+        .card-article-image {
+            width: 100%;
+            position: relative;
+            overflow: hidden;
+
+            img {
+                object-fit: cover;
+                height: 100%;
+                min-height: 200px;
+                max-height: 200px;
+                border-radius: 0 10px 0 10px;
+            }
+        }
+        .card-article-date {
+            font-size: 12px;
+            font-weight: 400;
+
+            .card-article-date-icon {
+                width: 15px;
+                height: 15px;
+            }
+        }
+        .card-article-title {
+            font-weight: 700;
+            font-size: 18px;
+            width: 14rem;
+            height: 7rem;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+        }
+        .card-article-read-more {
+            color: rgba(43, 43, 43, 1);
+            font-weight: 400;
+            font-size: 16px;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    }
+    @media (max-width: 768px) {
+        .card-article {
+            margin: 10px;
+        }
+    }
+</style>
